a936a4db865136fa52990e24e4e8a4836b160e95
Tom Werth <erfinder.tom@gmail.com>
2022-03-27 12:49:10 +0200

    code cleanup

--- src/main/java/de/tomino/serverjars/HashMD5.kt
+++ src/main/java/de/tomino/serverjars/HashMD5.kt
@@ -12,9 +12,9 @@ object HashMD5 {
     @Throws1(IOException::class, NoSuchAlgorithmException::class)
     operator fun get(path: Path?): String {
         val hash = StringBuilder()
-        for (bByte in MessageDigest.getInstance("MD5").digest(Files.readAllBytes(path))) {
-            hash.append(Character.forDigit((bByte.toInt() shr 4) and 0xF, 16))
-            hash.append(Character.forDigit((bByte and 0xF).toInt(), 16))
+        for (aByte in MessageDigest.getInstance("MD5").digest(Files.readAllBytes(path))) {
+            hash.append(Character.forDigit((aByte.toInt() shr 4) and 0xF, 16))
+            hash.append(Character.forDigit((aByte and 0xF).toInt(), 16))
         }
         return hash.toString()
     }
